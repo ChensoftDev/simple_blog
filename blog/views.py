25fda90800914e0003058c3fe707531fb26c7769
ADMIN <Sharlie.bkk@gmail.com>
2022-08-30 19:38:11 +1200

    Updated

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Category
+
+
+def index(request):
+    categories = Category.objects.all()
+    context = {"title": "my_homepage", "content": "my home page content",
+               "categories": categories}
+    return render(request, 'index.html', context)
